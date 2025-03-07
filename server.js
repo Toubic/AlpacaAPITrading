e47e6966d045047cae753be44534ceeef0b62645
TobiasSportson <toubic@gmail.com>
2025-03-07 22:45:52 +0100

    Work on news feed functionality and output of the news headlines

--- server.js
+++ server.js
@@ -3,26 +3,35 @@ const alpaca = new Alpaca(); // Looks at the .env file for the API keys automati
 
 const WebSocket = require("ws");
 
-const wss = new WebSocket("wss://paper-api.alpaca.markets/stream");
+const wss = new WebSocket("wss://stream.data.alpaca.markets/v1beta1/news");
+
 wss.on("open", () => {
-    console.log("Connected to Alpaca API");
+    console.log("Connected to the Alpaca API");
 
     const authMessage = {
         action: "auth",
         key: process.env.APCA_API_KEY_ID,
-        secret: process.env.APCA_API_SECRET,
+        secret: process.env.APCA_API_SECRET_KEY,
     };
-    wss.send(JSON.stringify(authMessage));
+
+    wss.send(JSON.stringify(authMessage)); 
 
     // Subscribe to all news feeds
     const subscribeMessage = {
         action: "subscribe",
-        news: ["*"],
+        news: ["*"] ,
     };
     wss.send(JSON.stringify(subscribeMessage));
 });
 
 wss.on("message", async (message) => {
-    console.log(message);
+    
+    const currentEvent = JSON.parse(message)[0];
+
+    console.log(currentEvent);
+
+    if(currentEvent.T === "n") {
+        console.log(currentEvent.headline);
+    }
 });
 
